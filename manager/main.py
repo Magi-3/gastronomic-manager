92c230627c120bfc58e9a821cb71ed4b7b7a87a0
Magi-3 <mourapastel@gmail.com>
2022-06-20 13:55:33 -0300

    Visual features

---
+++ manager/main.py
@@ -0,0 +1,17 @@
+import pyfiglet
+import inquirer
+from tabulate import tabulate
+
+is_logged = False
+table = [["spam",42,"cleiton"],["eggs",451, "jose"],["bacon",0]]
+headers = ["Receita", "Tipo", "Criador"]
+
+
+print(pyfiglet.figlet_format("ATOM"))
+print(tabulate(table, headers, tablefmt="fancy_grid"))
+
+selected_unit = inquirer.list_input("Escolha uma opção abaixo", choices=['Registrar', 'Logar'])
+if selected_unit == "Registrar":
+  pass
+elif selected_unit == "Logar":
+  pass

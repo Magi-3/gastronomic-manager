19355c2ec40db5393a64e57b5d721d841a455c9b
armandovinicius <armando.vinicius12@gmail.com>
2022-06-21 15:22:41 -0300

    Feat: User register done

--- manager/main.py
+++ manager/main.py
@@ -1,3 +1,16 @@
+def create_user():
+  registro = str(input('Digite seu primeiro nome, login e senha (separados por vírgula): ')).strip().split(',')
+  
+  usuario = {
+    'nome': registro[0].strip(),
+    'login': registro[1].strip(),
+    'senha': registro[2].strip()
+  }
+
+  return usuario
+
+users = []
+
 import pyfiglet
 import inquirer
 from tabulate import tabulate
@@ -6,16 +19,24 @@ is_logged = False
 table = [["spam",42,"cleiton"],["eggs",451, "jose"],["bacon",0]]
 headers = ["Receita", "Tipo", "Criador"]
 
-
 print(pyfiglet.figlet_format("ATOM"))
 print(tabulate(table, headers, tablefmt="fancy_grid"))
 
-selected_unit = inquirer.list_input("Escolha uma opção abaixo", choices=['Registrar', 'Logar'])
-if selected_unit == "Registrar":
-  pass
-elif selected_unit == "Logar":
-  pass
+while True:
+  if (is_logged == False):
+    selected_unit = inquirer.list_input("Escolha uma opção abaixo", choices=['Registrar', 'Logar'])
+    if selected_unit == "Registrar":
+      users.append(create_user())
+      print("Registro feito com sucesso! 👍")
+    elif selected_unit == "Logar":
+      dados = str(input("Digite seu login e senha: (separados por vírgula): ")).strip().split(',')
+      if (dados[0].strip() == users[0]['login'] and dados[1].strip() == users[0]['senha']):
+        is_logged = True
+        print("Login feito com sucesso! 👍")
+    print(pyfiglet.figlet_format("ATOM"))
+    print(tabulate(table, headers, tablefmt="fancy_grid"))
+  elif (is_logged):
+    selected_unit = inquirer.list_input("Escolha uma opção abaixo", choices=['Criar receita', 'Listar receitas', 'Sair']) 
 
 
-def soma_dois():
-  pass
+
